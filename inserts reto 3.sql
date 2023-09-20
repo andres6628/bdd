ff4c24e907dea13667ac7ebfd82b015c8e475400
Andres Villacres <andreslkf14@gmail.com>
2023-09-20 17:30:04 -0500

    inserts

---
+++ inserts reto 3.sql
@@ -0,0 +1,61 @@
+insert into productos values(1,'monitor','electronico',110,25);
+insert into productos values(2,'CPU','electronico',50,20);
+insert into productos values(3,'celular','electronico',500,5);
+insert into productos values(4,'tv box','electronico',30,100);
+insert into productos values(5,'smartwhatch','electronico',75,80);
+
+insert into productos values(6,'smart tv',null,110,25);
+insert into productos values(7,'audifonos',null,110,25);
+insert into productos values(8,'laptop',null,110,25);
+
+insert into cuentas values('00001','64789','2023/09/20',5);
+insert into cuentas values('00002','64788','2023/09/20',10);
+insert into cuentas values('00003','64787','2023/09/20',1);
+insert into cuentas values('00004','64786','2023/09/20',2);
+insert into cuentas values('00005','64785','2023/09/20',3);
+insert into cuentas values('00006','64784','2023/09/20',5);
+insert into cuentas values('00007','64783','2023/09/20',4);
+insert into cuentas values('00008','64782','2023/09/20',5);
+insert into cuentas values('00009','64781','2023/09/20',6);
+insert into cuentas values('00010','64780','2023/09/20',8);
+
+insert into estudiantes values('1234567890','carlos','jara','carlos@gmail.com','12-01-1997');
+insert into estudiantes values('1234567891','javier','jerez','javier@gmail.com','20-02-1997');
+insert into estudiantes values('1234567892','adrian','perez','adrian@gmail.com','12-03-1997');
+insert into estudiantes values('1234567893','pedro','navas','pedro@gmail.com','12-04-1998');
+insert into estudiantes values('1234567894','juan','navarrete','juean@gmail.com','12-05-1997');
+insert into estudiantes values('1234567895','maria','solis','maria@gmail.com','02-06-1999');
+insert into estudiantes values('1234567896','jose','sierra','jose@gmail.com','25-07-1995');
+insert into estudiantes values('1234567897','ana','solis','ana@gmail.com','04-08-1990');
+
+insert into registros_entrada values(1,'0000000001','2023-09-18','8:00');
+insert into registros_entrada values(2,'0000000002','2023-09-18','8:02');
+insert into registros_entrada values(3,'0000000003','2023-09-18','8:01');
+insert into registros_entrada values(4,'0000000001','2023-09-19','8:03');
+insert into registros_entrada values(5,'0000000002','2023-09-19','8:03');
+insert into registros_entrada values(6,'0000000003','2023-09-19','8:03');
+insert into registros_entrada values(7,'0000000001','2023-09-20','8:10');
+insert into registros_entrada values(8,'0000000002','2023-09-20','8:08');
+insert into registros_entrada values(9,'0000000003','2023-09-20','8:00');
+insert into registros_entrada values(10,'0000000001','2023-09-21','8:00');
+
+insert into videojuegos values(1,'fifa23','pc',5);
+insert into videojuegos values(2,'resident evil','pc',5);
+insert into videojuegos values(3,'pes 23','xbox',4);
+insert into videojuegos values(4,'outlast','play station 5',5);
+insert into videojuegos values(5,'mario','pc',5);
+
+insert into videojuegos values(6,'crash',null,null);
+insert into videojuegos values(7,'mariokart',null,null);
+insert into videojuegos values(8,'nba',null,null);
+
+insert into transacciones values(1,'00001',85.4,'D','2023-09-18','12:00');
+insert into transacciones values(2,'00002',100,'D','2023-09-18','12:05');
+insert into transacciones values(3,'00003',200,'D','2023-09-18','12:07');
+insert into transacciones values(4,'00001',100,'D','2023-09-18','12:08');
+insert into transacciones values(5,'00002',5,'D','2023-09-18','12:20');
+insert into transacciones values(6,'00003',10,'C','2023-09-18','12:25');
+insert into transacciones values(7,'00001',4,'C','2023-09-18','12:30');
+insert into transacciones values(8,'00002',4,'C','2023-09-18','13:00');
+insert into transacciones values(9,'00003',3,'C','2023-09-18','13:10');
+insert into transacciones values(10,'00001',15,'C','2023-09-18','13:15');
